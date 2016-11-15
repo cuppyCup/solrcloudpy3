a8e59aabcc2e4af612f46d20ebb0e0000afdc834
Matthew Barrett <matthew.robert.barrett@gmail.com>
2016-11-14 19:10:47 -0500

    Correcting author information.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='solrcloudpy3',
       version=get_version(),
       description='Solr CLOUD API for Python 3.5 and Solr 6.3.0.  This is a fork of the code base posted '
                   + 'at https://github.com/solrcloudpy/solrcloudpy .',
-      author='Matthew Barrett & the Open Source Community',
+      author='Didier Deshommes, Robert Elwell, Matthew Barrett',
       author_email='matthew.robert.barrett@gmail.com',
       url='https://www.github.com/cuppyCup/solrclodupy3/',
       license='LICENSE.txt',
